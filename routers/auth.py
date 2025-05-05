5a592c31c44b41c90e0d1cc7da93e3b5d3ff5396
Rosa Soto <rsh456@hotmail.com>
2025-05-05 17:28:30 -0500

    feat: refactor authentication method by using oAuth

--- routers/auth.py
+++ routers/auth.py
@@ -1,23 +1,38 @@
 from typing import Annotated
 
-from fastapi import Depends, HTTPException
-from fastapi.security import HTTPBasicCredentials, HTTPBasic
+from fastapi import Depends, HTTPException, APIRouter
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlmodel import Session, select
 from schemas import UserOutput, User
 from db import get_session
 from starlette import status
 
-security = HTTPBasic()
+URL_PREFIX = "/api/auth"
+router = APIRouter(prefix=URL_PREFIX)
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl=f"{URL_PREFIX}/token")
 
-def get_current_user(credentials: Annotated[HTTPBasicCredentials, Depends(security)] ,
+def get_current_user(token: Annotated[str, Depends(oauth2_scheme)] ,
                     session: Annotated[Session, Depends(get_session)]) -> UserOutput:
     
-    query = select(User).where(User.username == credentials.username)
+    # For this delivery the token will contain only the username
+    query = select(User).where(User.username == token)
     user = session.exec(query).first()
-    if user and user.verify_password(credentials.password):
+    if user:
         return UserOutput.model_validate(user)
     else:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Username or password incorrect",
+            headers={"WWW-Authenticate": "Bearer"},
         )
+    
+@router.post("/token")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+                session: Annotated[Session, Depends(get_session)]):
+    query = select(User).where(User.username == form_data.username)
+    user = session.exec(query).first()
+    if user and user.verify_password(form_data.password):
+        return {"access_token": user.username, "token_type": "bearer"}
+    else:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
